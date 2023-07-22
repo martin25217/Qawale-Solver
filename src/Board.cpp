f99363aa05c653f687985ad30fcd9aa0e288f2da
martin25217 <martinrogosic@gmail.com>
2023-07-22 16:42:18 +0200

    Relocated some functions and finished the input validator

--- src/Board.cpp
+++ src/Board.cpp
@@ -27,4 +27,38 @@ class Board{
         board[3][3].enqueue(yellow);
     }
 
+    bool validateInput(input userInput){
+        if(userInput.startColumn < 0 || userInput.startColumn > 3) return false;
+        if(userInput.startRow < 0 || userInput.startRow > 3) return false;
+
+        if(board[userInput.startRow][userInput.startColumn].size() == 0) return false;
+
+        int directionLength;
+        for(directionLength = 1; *(userInput.direction + directionLength) != '\0'; directionLength++);
+        if(directionLength != board[userInput.startRow][userInput.startColumn].size() + 1) return false;
+
+        int rowHelper = userInput.startRow;
+        int columnHelper = userInput.startColumn;
+        char *directionHelper = userInput.direction;
+
+        do{
+            if(*directionHelper != 'l' && *directionHelper != 'r'
+            && *directionHelper != 'u' && *directionHelper != 'd') return false;
+
+            if(*directionHelper == 'l') rowHelper--;
+            if(*directionHelper == 'r') rowHelper++;
+
+            if(*directionHelper == 'u') columnHelper--;
+            if(*directionHelper == 'd') columnHelper++;
+
+            if(columnHelper < 0 || columnHelper > 3) return false;
+            if(rowHelper < 0 || rowHelper > 3) return false;
+
+            directionHelper++;
+
+        }while(*directionHelper != '\0');
+
+        return true;
+}
+
 };
--- src/Input.cpp
+++ src/Input.cpp
@@ -1,15 +1,20 @@
 #include<iostream>
 #include<string>
 
+typedef struct{
+    int startRow;
+    int startColumn;
+    char* direction;
+}input;
 
-char* splitString(std::string input){
-    char *result = (char*) malloc((input.size()+1)*sizeof(char));
 
-    for(int i = 0; i <  input.size(); i++){
-        *(result + i) = input.at(i);
-    }
-
-    *(result + input.size()) = '\0';
+input convertStringToInput(char* userInputString){
+    input result;
+    result.startRow = *userInputString- 48;
+    result.startColumn = *userInputString - 48;
+    result.direction = userInputString + 3;
     return result;
 }
+
+
 
