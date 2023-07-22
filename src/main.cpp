bda0a1afc66f4d51e313343e7b30afbcad14f5a5
blueFlower11 <133530289+blueFlower11@users.noreply.github.com>
2023-07-22 20:16:28 +0200

    it prints

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
 #include"../src/Board.cpp"
+#include <string>
+#include <iostream>
 
+using namespace std;
 
 int main(void){
     Board *board = new Board();
+
+    int maxHeight = board->maxQ();
+
+    for(int i = 0; i < 16; i++){
+        Queue<Colors> b = board->printB(i);
+        cout << b.printQ(maxHeight);
+        if(i % 4 != 3) cout << '|';
+        if(i % 4 == 3) cout << endl;
+    }
 }
