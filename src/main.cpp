68bd1c6ada1523d584b6bbc1fbfbb98d1525b4c6
blueFlower11 <133530289+blueFlower11@users.noreply.github.com>
2023-07-29 13:47:02 +0200

    igraj

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,37 @@
 int main(void){
 
     Board *board = new Board();
+board->printGameState();
 
-    board->printGameState();
-    board->play("00|ddd", red);
-    board->printGameState(); 
+    int win;
+
+    do{
+        std::cout << "Player ";
+        if(turn % 2 == 0) std::cout << "red";
+        else std::cout << "white";
+        std::cout << ": enter your input in the form of RowColumn|Directions"<< std::endl;
+        std::string inputString;
+        std::cin >> inputString;
+        std::cout << "Your move is: ";
+        std::cout << inputString << std::endl;
+
+        bool played;
+
+        if(turn % 2 == 0) played = board->play(inputString, red);
+        else played = board->play(inputString, white);
+
+        if(played == false) turn -= 1;
+
+        board->printGameState();
+
+        win = board->winCheck();
+        if(win == 1) std::cout << "WIN: Winner is white player!" << std::endl;
+        else if(win == 2) std::cout << "WIN: Winner is red player!" << std::endl;
+        else if(win == 3) std::cout << "No winner: game is undecided." << std::endl;
+
+        turn += 1;
+    }while(win == 0 && turn < 17);
+
+    if(win == 0) std::cout << "No winner: game is undecided." << std::endl;
+    
 }
